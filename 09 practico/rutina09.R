969ad3ac9700224fd12a4c8442cf6dd654f413c7
santiagombv <santiagombv@gmail.com>
2024-08-28 00:01:59 -0300

    update tp 8-10

--- 09 practico/rutina09.R
+++ 09 practico/rutina09.R
@@ -1,14 +1,14 @@
 ## Caso 1. Modelos lineales randomizados.
-
-# complete la ruta al directorio en ...
-vacas <- read.table(".../vacas.txt", header = TRUE)
+library(performance)
+library(ggplot2)
+vacas <- read.table("vacas.txt", header = TRUE)
 
 fit <- lm(fat ~ breed, data = vacas)
-summary(fit)
 anova(fit)
-layout(matrix(1:4, 2, 2))
-plot(fit)  # examinar violación de supuestos
-layout(1)
+
+check_model(fit)
+check_heteroscedasticity(fit)
+check_normality(fit)
 
 # Guardar las F observadas
 Fobs <- anova(fit)[1, 4]
@@ -28,36 +28,35 @@ rd.aov(Y = vacas$fat, X = vacas$breed)
 
 # Realizar réplicas para obtener la distribución de pseudo F
 pseudoF <- replicate(1000, rd.aov(Y = vacas$fat, X = vacas$breed))
-PF <- t(pseudoF)	#transposición para que cada F quede en una columna
+PF <- data.frame(pseudoF)	#transposición para que cada F quede en una columna
 
 # histograma para ver los resultados simulados y los observados
-hist(PF)
-abline(v = Fobs, col = "red")
-
-hist(PF, xlim=c(0, 51))
-abline(v = Fobs, col = "red")
+ggplot(data=PF, aes(x = pseudoF)) + 
+  geom_histogram() + 
+  geom_vline(xintercept = Fobs, color = "red")
 
 # valores P
-P.breed <- length(PF[PF >= Fobs]) / length(PF)
+P.breed <- length(pseudoF[pseudoF >= Fobs]) / length(pseudoF)
 P.breed  
-# como nuestra simulación llegó hasta mil 
-# no podemos asegurar que sea 0, sino P<0.001
+
+# como nuestra simulación llegó hasta 1000
+# no podemos asegurar que sea 0, sino que P es al menos <0.001
 
 #############################################################
 
 ## Caso 2: Un Monte Carlo sencillo.
-
-# complete la ruta al directorio en ...
-esp <- read.table(".../espMC.txt", header = TRUE)
-plot(y ~ x, data=esp)
+esp <- read.table("espMC.txt", header = TRUE)
+plot(y ~ x, data=esp, pch = 19, col = "purple4")
 
 # calcular las distancias euclídeas entre las filas de la base de datos
 # como parámetro observado se elige la media  de todas las distancias 
+# (¿por qué elijo ese parámetro?)
 
-Dobs<-mean(dist(esp))		#revisar la ayuda de la función dist
+Dobs <- mean(dist(esp))		#revisar la ayuda de la función dist
 Dobs
 
-# Realizamos una simulación de la distribución de los árboles según una #distribución uniforme
+# Realizamos una simulación de la distribución de los árboles según una 
+# distribución uniforme
 X.azar <- runif(n = nrow(esp), min = 0, max = 2)		
 Y.azar <- runif(n = nrow(esp), min = 0, max = 2)
 plot(X.azar, Y.azar)
@@ -75,6 +74,7 @@ for (i in 1:1000) {
 # visualizamos los resultados con un histograma
 hist(Ds)
 abline(v = Dobs, col = "red")
+
 # calcular el valor P
 P <- length(Ds[Ds < Dobs]) / length(Ds)
 P
--- 10 practico/rutina10.R
+++ 10 practico/rutina10.R
@@ -1,9 +1,8 @@
 ##Caso1
+library(boot)
+cyc <- read.table("cyclop.txt", header = TRUE)
 
-# complete la ruta al directorio en ...
-ulm <- read.table(".../ulmifolia.txt", header = TRUE)
-
-reg <- lm(semillas ~ flores + nectar + tamano, data = ulm)
+reg <- lm(pol.exp ~ flores + nectario, data = cyc)
 summary(reg)
 
 layout(matrix(1:4, 2, 2))
@@ -14,33 +13,26 @@ layout(1)
 Bobs <- reg$coeff
 Bobs
 
-# BOOTSTRAP 
-library(boot)
-
 # PASO 1: crear una función para "bootstrapear"
 REG <- function(datos, indices) {
   newdata <- datos[indices, ]  #sobre esta línea actuará la función boot  
-  fit <- lm(semillas ~ flores + nectar + tamano, data = newdata)
+  fit <- lm(pol.exp ~ flores + nectario, data = newdata)
   fit$coeff
 }
 
 # PASO 2: el bootstrap en sí
-?boot
-
-boot.reg <- boot(ulm, REG, 9999)
+boot.reg <- boot(cyc, REG, 9999)
 boot.reg
 summary(boot.reg)
 
 # histograma de los resultados
 layout(matrix(1:4, 2, 2))
-hist(boot.reg$t[, 1]) 
+hist(boot.reg$t[, 1], main = "intercept") 
 abline(v = Bobs[1], col="red") 
-hist(boot.reg$t[, 2]) 
+hist(boot.reg$t[, 2], main = "flores") 
 abline(v = Bobs[2], col="red") 
-hist(boot.reg$t[, 3]) 
+hist(boot.reg$t[, 3], main = "nectario") 
 abline(v = Bobs[3], col = "red") 
-hist(boot.reg$t[,4]) 
-abline(v = Bobs[4], col = "red") 
 layout(1)
 
 ## PASO 3: intervalos de confianza
@@ -63,7 +55,7 @@ int2.95
 int2.99
 int2.999
 
-# intervalos para la pendiente de "nectar"
+# intervalos para la pendiente de "nectario"
 int3.90 <- boot.ci(boot.reg, conf = 0.90, type = "bca", index = 3)
 int3.95 <- boot.ci(boot.reg, conf = 0.95, type = "bca", index = 3)
 int3.99 <- boot.ci(boot.reg, conf = 0.99, type = "bca", index = 3)
@@ -74,38 +66,21 @@ int3.95
 int3.99
 int3.999
 
-# intervalos para la pendiente de "tamaño"
-int4.90 <- boot.ci(boot.reg, conf = 0.90, type = "bca", index = 4)
-int4.95 <- boot.ci(boot.reg, conf = 0.95, type = "bca", index = 4)
-int4.99 <- boot.ci(boot.reg, conf = 0.99, type = "bca", index = 4)
-int4.999 <- boot.ci(boot.reg, conf = 0.999, type = "bca", index = 4)
+# PASO 4: plots de diagnóstico (salteamos el de intercepto)
+plot(boot.reg, index = 2, jack = T)
+plot(boot.reg, index = 3, jack = T)
 
-int4.90
-int4.95
-int4.99
-int4.999
-
-# PASO 4: Un plot de diagnóstico: el jack.after.boot
+# el jack-after-boot en más detalle
 # influencia de las observaciones individuales sobre la pendiente de 
 # "flores"
+
 jack.after.boot(boot.reg, index = 2) 
 
-# ídem sobre la pendiente de "néctar"
+# ídem sobre la pendiente de "nectario"
 jack.after.boot(boot.reg, index = 3) 
-
-
-# ídem sobre la pendiente de "tamaño"
-jack.after.boot(boot.reg, index = 4) 
 
 # la línea horizontal es una medida de la influencia de cada observación
 # la línea vertical presenta los percentiles 5, 10, 16, 50, 84, 90 y 95 de 
 # la distribución del coeficiente
-
-# Otro plot de diagnóstico alternativo puede realizarse con plot
-# esto nos puede ayudar a decidir el tipo de intervalo
-
-plot(boot.reg, index = 2, jack = T)
-plot(boot.reg, index = 3, jack = T)
-plot(boot.reg, index = 4, jack = T)
 
 #### END ####
